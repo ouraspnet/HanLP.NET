528c1016fd8689e928c7a6038c45a09847c42c13
yangxiaodong <yangxiaodong1214@126.com>
2017-04-19 17:13:02 +0800

    添加 IO 帮助类

---
+++ src/HanLP.Net/Corpus/IO/IOUtil.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace HanLP.Net.Corpus.IO
+{
+    public class IOUtil
+    {
+        public static byte[] ReadBytes(string path) {
+            using (var fileStream = new FileStream(path, FileMode.Create)) {
+                byte[] bytes = new byte[fileStream.Length];
+                fileStream.Read(bytes, 0, bytes.Length);
+                return bytes;
+            }
+        }
+
+        public static StreamReader NewInputStream(string path, int bufferSize = 4096) {
+
+            var fileSteam = new FileStream(path, FileMode.Create, 
+                FileAccess.Read, FileShare.Read, bufferSize);
+
+            return new StreamReader(fileSteam);
+        }
+
+        public static StreamWriter NewOutputStream(string path, int bufferSize = 4096) {
+
+            var fileSteam = new FileStream(path, FileMode.Open, 
+                FileAccess.ReadWrite, FileShare.ReadWrite, bufferSize);
+
+            return new StreamWriter(fileSteam);
+        }
+    }
+
+
+}
