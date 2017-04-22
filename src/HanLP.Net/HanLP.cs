4bb94f76c8560c7a64a0dd88e07207a39a3f45aa
yangxiaodong <yangxiaodong1214@126.com>
2017-04-22 17:45:28 +0800

    部分未完成

--- src/HanLP.Net/HanLP.cs
+++ src/HanLP.Net/HanLP.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using HanLP.Net.Configures;
 
 namespace HanLP.Net
 {
-
-    public class HanLP
+    public static class HanLP
     {
+        public static HanLPFileConfig Config { get; set; }
 
-        public HanLP(HanLPConfiguration config) {
 
-        } 
 
 
 
     }
+
+    //static void Main(string[] args) {
+    //   // HanLP.Config.CoreDictionaryPath
+    //}
+     
 }
---
+++ src/HanLP.Net/Seg/Term.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HanLP.Net.Corpus.Tag;
+using HanLP.Net.Utility;
+
+namespace HanLP.Net.Seg
+{
+    /// <summary>
+    /// 一个单词，用户可以直接访问此单词的全部属性
+    /// </summary>
+    public class Term
+    {
+        /// <summary>
+        /// 词语
+        /// </summary>
+        private string word;
+
+        /// <summary>
+        /// 词性
+        /// </summary>
+        private Nature nature;
+
+        /// <summary>
+        /// 在文本中的起始位置（需开启分词器的offset选项）
+        /// </summary>
+        public int offset;
+
+        /// <summary>
+        /// 构造一个单词
+        /// </summary>
+        public Term(string word, Nature nature) {
+            this.Word = word;
+            this.nature = nature;
+        }
+
+
+        public override string ToString() {
+            if (HanLP.Config.ShowTermNature)
+                return Word + "/" + nature;
+            return Word;
+        }
+
+        public int Length { get { return Word.Length; } }
+
+        /// <summary>
+        /// 词语
+        /// </summary>
+        public string Word { get => word; set => word = value; }
+
+        /// <summary>
+        /// 词性
+        /// </summary>
+        public Nature Nature { get => nature; set => nature = value; }
+
+        /// <summary>
+        /// 获取本词语在HanLP词库中的频次
+        /// </summary>
+        /// <returns>频次，0代表这是个OOV</returns>
+        public int GetFrequency() {
+            // return LexiconUtility.GetFrequency(Word);
+            throw new NotImplementedException();
+        }
+    }
+
+}
